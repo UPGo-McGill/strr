16d3ff8dd19d034830c67d10e4a076b6e5868f9a
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-04-26 14:37:39 -0400

    Test notes

--- tests/testthat/test-strr_raffle.R
+++ tests/testthat/test-strr_raffle.R
@@ -1,3 +1,9 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+
+# Should have tests for the full strr_raffle function, and for the important
+# sub functions
+
+# Need a toy dataset to do the tests on
