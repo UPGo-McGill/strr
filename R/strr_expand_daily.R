3291a9c6e4015d704c4208415aa4dab821cddeb9
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-08-11 22:05:14 -0400

    Performance optimized; error-checking added

--- R/strr_expand_daily.R
+++ R/strr_expand_daily.R
@@ -1,26 +1,76 @@
-strr_expand_daily <- function(daily, start = NULL, end = NULL) {
+strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
 
-  # Expand each date range to one-row-per-date
-  daily <-
-    setDT(daily)[, list(apid = apid,
-                        date = seq(start_date, end_date, by = "day"),
-                        status = status, rid = rid, cost = cost),
-                 by = 1:nrow(daily)]
+  ## ERROR CHECKING AND ARGUMENT INITIALIZATION
 
-  # Optionally trim based on start date
+  # Check that cores is an integer > 0
+
+  cores <- floor(cores)
+  if (cores <= 0) {
+    stop("The argument `cores` must be a positive integer.")
+  }
+
+  # Check that dates are coercible to date class, then coerce them
+
   if (!is.null(start)) {
-    start <- as.Date(start)
-    daily <- daily[date >= start]
-  }
+    start <- tryCatch(as.Date(start), error = function(e) {
+      stop(paste0('The value of `start`` ("', start,
+                  '") is not coercible to a date.'))
+    })}
 
-  # Optionally trim based on end date
   if (!is.null(end)) {
-    end <- as.Date(end)
-    daily <- daily[date <= end]
+    end <- tryCatch(as.Date(end), error = function(e) {
+      stop(paste0('The value of `end` ("', end,
+                  '") is not coercible to a date.'))
+    })}
+
+
+  ## PREPARE DATE FIELD
+
+  daily <-
+    daily %>%
+    mutate(date = map2(start_date, end_date, ~{.x:.y}))
+
+
+  ## SINGLE-CORE VERSION
+
+  if (cores == 1) {
+
+    daily <-
+      daily %>%
+      unnest() %>%
+      mutate(date = as.Date(date, origin = "1970-01-01")) %>%
+      select(property_ID, date, status:res_id)
+
+  ## MULTI-CORE VERSION
+
+  } else {
+
+    daily_list <-
+      split(daily, 0:(nrow(daily) - 1) %/% ceiling(nrow(daily) / 100))
+
+    daily <-
+      pblapply(daily_list, function(x) {
+        x %>%
+          unnest() %>%
+          mutate(date = as.Date(date, origin = "1970-01-01")) %>%
+          select(property_ID, date, status:res_id)
+      }) %>%
+      bind_rows()
   }
 
-  # Output tibble
-  daily %>%
-    as_tibble() %>%
-    select(-nrow)
+
+  ## OPTIONALLY TRIM BASED ON START/END DATE
+
+  if (!is.null(start)) {
+    daily <- filter(daily, date >= start)
+  }
+
+  if (!is.null(end)) {
+    daily <- filter(daily, date <= end)
+  }
+
+
+  ## OUTPUT DATA FRAME
+
+  return(daily)
 }
