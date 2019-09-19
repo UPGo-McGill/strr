7b0a847c23dd9f3750302462cc2143fdb6920c18
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-09-19 07:06:31 -0400

    strr_expand_daily function working, still needs documentation

--- R/strr_expand_daily.R
+++ R/strr_expand_daily.R
@@ -1,3 +1,35 @@
+#' Function to expand compressed STR daily tables
+#'
+#' \code{strr_expand_daily} takes a compressed STR daily file and expands it to
+#' a one-row-per-date format.
+#'
+#' A function for probablistically assigning STR listings to administrative
+#' geographies (e.g. census tracts) based on reported latitude/longitude.
+#' The function works by combining a known probability density function (e.g.
+#' Airbnb's spatial obfuscation of listing locations) with an additional source
+#' of information about possible listing locations--either population or housing
+#' densities.
+#'
+#' @param daily An sf or sp point-geometry object, in a projected coordinate
+#'   system.
+#' @param cores A positive integer scalar. How many processing cores should be
+#'   used to perform the computationally intensive numeric integration step?
+#' @return The output will be the input points object with a new `winner` field
+#'   appended. The `winner` field specifies which polygon from the polys object
+#'   was probabilistically assigned to the listing, using the field identified
+#'   in the `poly_ID` argument. If diagnostic == TRUE, a `candidates` field will
+#'   also be appended, which lists the possible polygons for each point, along
+#'   with their probabilities.
+#' @importFrom dplyr %>% as_tibble enquo filter group_by left_join mutate
+#' @importFrom dplyr select summarize
+#' @importFrom methods is
+#' @importFrom rlang := .data
+#' @importFrom sf st_area st_as_sf st_buffer st_coordinates st_crs
+#' @importFrom sf st_drop_geometry st_intersection st_set_agr st_sfc
+#' @importFrom sf st_transform
+#' @importFrom stats dnorm
+#' @export
+
 strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
 
   ## ERROR CHECKING AND ARGUMENT INITIALIZATION
@@ -11,13 +43,13 @@ strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
 
   # Check that dates are coercible to date class, then coerce them
 
-  if (!is.null(start)) {
+  if (!missing(start)) {
     start <- tryCatch(as.Date(start), error = function(e) {
       stop(paste0('The value of `start`` ("', start,
                   '") is not coercible to a date.'))
     })}
 
-  if (!is.null(end)) {
+  if (!missing(end)) {
     end <- tryCatch(as.Date(end), error = function(e) {
       stop(paste0('The value of `end` ("', end,
                   '") is not coercible to a date.'))
@@ -37,9 +69,9 @@ strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
 
     daily <-
       daily %>%
-      unnest() %>%
+      unnest(cols = c(date)) %>%
       mutate(date = as.Date(date, origin = "1970-01-01")) %>%
-      select(property_ID, date, status:city)
+      select(property_ID, date, everything(), -start_date, -end_date)
 
   ## MULTI-CORE VERSION
 
@@ -51,9 +83,9 @@ strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
     daily <-
       pblapply(daily_list, function(x) {
         x %>%
-          unnest() %>%
+          unnest(cols = c(date)) %>%
           mutate(date = as.Date(date, origin = "1970-01-01")) %>%
-          select(property_ID, date, status:city)
+          select(property_ID, date, everything(), -start_date, -end_date)
       }, cl = cores) %>%
       bind_rows()
   }
@@ -61,11 +93,11 @@ strr_expand_daily <- function(daily, start = NULL, end = NULL, cores = 1) {
 
   ## OPTIONALLY TRIM BASED ON START/END DATE
 
-  if (!is.null(start)) {
+  if (!missing(start)) {
     daily <- filter(daily, date >= start)
   }
 
-  if (!is.null(end)) {
+  if (!missing(end)) {
     daily <- filter(daily, date <= end)
   }
 
