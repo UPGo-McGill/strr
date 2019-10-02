55bc851aed2be3b7f0b3bc84439bfbaacb16db88
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-10-02 08:12:33 -0400

    Bug fix

--- R/strr_compress.R
+++ R/strr_compress.R
@@ -176,7 +176,7 @@ strr_compress <- function(.data, cores = 1, chunks = TRUE, quiet = FALSE) {
 
     daily_list <- split(.data, .data$property_ID)
 
-    if (length(daily_list) > 10000 & chunks = TRUE) {
+    if (length(daily_list) > 10000 & chunks == TRUE) {
       daily_list <- purrr::map(1:10000, function(i) {
         bind_rows(
           daily_list[(floor(as.numeric(length(daily_list)) * (i - 1) / 10000) +
