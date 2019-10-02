8c06f6a9bb0e36cdd365e0aff018ed87f78ef3a2
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-10-02 13:30:47 -0400

    Added progress messages to strr_expand, and increased the number of chunks for multicore processing from 1000 to 10,000

--- R/strr_expand.R
+++ R/strr_expand.R
@@ -18,14 +18,20 @@
 #'   latest date present in the data will be used.
 #' @param cores A positive integer scalar. How many processing cores should be
 #'   used to perform the computationally intensive numeric integration step?
-#' @return A table with one row per date and all other fields returned unaltered.
+#' @param quiet A logical scalar. Should the function execute quietly, or should
+#' it return status updates throughout the function (default)?
+#' @return A table with one row per date and all other fields returned
+#' unaltered.
 #' @importFrom dplyr %>% bind_rows filter mutate select
 #' @importFrom purrr map2
 #' @importFrom rlang .data
 #' @importFrom tidyr unnest
 #' @export
 
-strr_expand <- function(.data, start = NULL, end = NULL, cores = 1) {
+strr_expand <- function(.data, start = NULL, end = NULL, cores = 1,
+                        quiet = FALSE) {
+
+  time_1 <- Sys.time()
 
   ## ERROR CHECKING AND ARGUMENT INITIALIZATION
 
@@ -53,6 +59,9 @@ strr_expand <- function(.data, start = NULL, end = NULL, cores = 1) {
 
   ## PREPARE DATE FIELD
 
+  if (!quiet) {message("Preparing new date field (",
+                       substr(Sys.time(), 12, 19), ").")}
+
   .data <-
     .data %>%
     mutate(date = map2(.data$start_date, .data$end_date, ~{.x:.y}))
@@ -61,6 +70,9 @@ strr_expand <- function(.data, start = NULL, end = NULL, cores = 1) {
   ## SINGLE-CORE VERSION
 
   if (cores == 1) {
+
+    if (!quiet) {message("Beginning expansion (",
+                         substr(Sys.time(), 12, 19), ").")}
 
     .data <-
       .data %>%
@@ -71,10 +83,16 @@ strr_expand <- function(.data, start = NULL, end = NULL, cores = 1) {
 
   } else {
 
-    daily_list <-
-      split(.data, ceiling(1:nrow(.data)/1000))
+    if (!quiet) {message("Splitting table for multi-core processing (",
+                         substr(Sys.time(), 12, 19), ").")}
 
-    .data <-
+    daily_list <-
+      split(.data, ceiling(1:nrow(.data)/10000))
+
+    if (!quiet) {message("Beginning expansion (",
+                         substr(Sys.time(), 12, 19), ").")}
+
+        .data <-
       pbapply::pblapply(daily_list, function(x) {
         x %>%
           unnest(cols = c(date)) %>%
@@ -109,6 +127,13 @@ strr_expand <- function(.data, start = NULL, end = NULL, cores = 1) {
 
 
   ## OUTPUT DATA FRAME
+
+  total_time <- Sys.time() - time_1
+
+  if (!quiet) {message("Expansion complete (",
+                       substr(Sys.time(), 12, 19), "). Total time: ",
+                       substr(total_time, 1, 5), " ",
+                       attr(total_time, "units"), ".")}
 
   return(.data)
 }
