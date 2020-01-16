3f3c504000aed722312f424364a93966fffc66d3
dwachsmuth <david.wachsmuth@mcgill.ca>
2020-01-16 10:54:51 -0500

    Rewrite of strr_process_multi to use data.table for much faster performance

--- R/strr_process_multi.R
+++ R/strr_process_multi.R
@@ -3,73 +3,91 @@
 #' \code{strr_process_multi} takes processed daily tables and produces summary
 #' tables listings host activity per day.
 #'
-#' A function for cleaning raw daily activity tables from AirDNA and preparing
-#' them for compression into the UPGo format. The function also produces error
-#' files which identify possible corrupt or missing lines in the input file.
+#' A function for aggregating daily activity tables by host, listing type, and
+#' housing status, in order to determine the extent of multilisting activity
+#' in a given STR dataset. This function will typically be run on the output of
+#' \code{\link{strr_process_daily}} in preparation for compression with
+#' \code{\link{strr_compress}}.
 #'
-#' @param .daily An unprocessed daily table in the raw AirDNA format, with
-#' either ten or six fields.
+#' @param daily An processed daily table in the UPGo format (probably produced
+#' with \code{\link{strr_process_daily}}), with either ten or six fields.
 #' @param quiet A logical scalar. Should the function execute quietly, or
 #' should it return status updates throughout the function (default)?
-#' @param ... Additional arguments to pass to component functions. Arguments
-#' must be named. Currently the only valid argument is `multiplier`, to control
-#' the number of list elements that will be prepared for parallel processing.
-#' The default value is 4, which means the input file will be divided into
-#' \code{(4 * #' workers)} number of elements, where `workers` is set in
-#' \code{future::plan}. Higher values mean more elements, which will slow down
-#' processing time but reduce RAM usage. Lower values mean the inverse.
 #' @return A processed multilisting table, ready for compression with
 #' \code{\link{strr_compress}}.
-#' @importFrom dplyr %>% anti_join bind_rows filter inner_join mutate pull
-#' @importFrom dplyr select
+#' @importFrom data.table setDT
+#' @importFrom dplyr %>% group_split
 #' @importFrom rlang .data
 #' @importFrom tibble as_tibble
 #' @export
 
-strr_process_multi <- function(.daily, quiet = FALSE, ...) {
+strr_process_multi <- function(daily, quiet = FALSE) {
 
   time_1 <- Sys.time()
 
-  ## Error checking and initialization
+  ### Error checking and initialization ########################################
+
+  ## data.table setup
+
+  .datatable.aware = TRUE
+
+  host_ID <- status <- date <- listing_type <- housing <- NULL
+
+
+  ## Error checking
 
   # Remove future global export limit
   options(future.globals.maxSize = +Inf)
   on.exit(.Options$future.globals.maxSize <- NULL)
 
+  # Check that table is a data frame
+  if (!inherits(daily, "data.frame")) {
+    stop("The object supplied to the `daily` argument must be a data frame.")
+  }
 
-  ## Trim daily table
+  # Check that table is daily
+  if (!inherits(daily, "strr_multi") | names(daily)[1] != "property_ID") {
+    stop("Input table must be of class `strr_daily`.")
+  }
+
+  # Check that quiet is a logical
+  if (!is.logical(quiet)) {
+    stop("The argument `quiet` must be a logical value (TRUE or FALSE).")
+  }
+
+
+  ### Trim daily table #########################################################
 
   helper_progress_message("Trimming daily table to valid entries.")
 
-  .daily <-
-    .daily %>%
-    filter(.data$status != "U", !is.na(.data$host_ID)) %>%
-    # Remove unnecessary fields to reduce memory usage
-    select(.data$host_ID, .data$date, .data$listing_type, .data$housing)
+  setDT(daily)
+
+  daily <-
+    daily[status != "U" & !is.na(host_ID),
+          .(host_ID, date, listing_type, housing)]
 
   # Save nrow for final validity check
-  daily_check <- nrow(.daily)
+  daily_check <- nrow(daily)
 
 
   ## Produce list for processing
 
   data_list <-
-    .daily %>%
+    daily %>%
     group_split(.data$host_ID) %>%
-    helper_table_split(...)
+    helper_table_split()
 
 
-  ## Produce multilisting table
+  ### Produce multilisting table ###############################################
 
   helper_progress_message("Beginning processing, using {helper_plan()}.")
 
   multi <-
     data_list %>%
     future_map_dfr(~{
-      .x %>%
-        count(.data$host_ID, .data$date, .data$listing_type, .data$housing) %>%
-        ungroup() %>%
-        rename(count = n)
+      setDT(.x)
+      .x[,.(count = .N), by = .(host_ID, date, listing_type, housing)] %>%
+        as_tibble()
     },
     # Suppress progress bar if quiet == TRUE or the plan is remote
     .progress = helper_progress(quiet))
