58f7d486b011ad252e194e15807d2f12b1538a34
dwachsmuth <david.wachsmuth@mcgill.ca>
2020-01-19 09:30:07 -0500

    Bug fix to missing_rows calculation in strr_process_daily

--- R/strr_process_daily.R
+++ R/strr_process_daily.R
@@ -188,8 +188,9 @@ strr_process_daily <- function(daily, property, keep_cols = FALSE,
   # Faster and less memory-intensive to split up
   missing_rows <-
     daily[, .(count = .N, low = min(date), high = max(date)), by = "property_ID"
-          ][, dif := as.integer(high - low) + 1
-            ][dif > 0]
+          ][, dif := as.integer(high - low) - count + 1
+            ][c("low", "high") := NULL
+              ][dif > 0]
 
   helper_progress_message("Missing rows identified.")
 
