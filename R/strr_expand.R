c8eccae05de04061ec722d72417d7754a46d6c46
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-05-25 21:33:00 -0400

    strr_expand further cleaned up to reduce dependencies and simplify logic

--- R/strr_expand.R
+++ R/strr_expand.R
@@ -15,48 +15,24 @@
 #' it return status updates throughout the function (default)?
 #' @return A table with one row per date and all other fields returned
 #' unaltered.
-#' @importFrom data.table last setDT setDTthreads setnames
-#' @importFrom dplyr %>% as_tibble everything filter left_join mutate select
-#' @importFrom dplyr slice
-#' @importFrom furrr future_map_dfr
 #' @importFrom rlang .data
 #' @export
 
 strr_expand <- function(data, quiet = FALSE) {
 
-  start_time <- Sys.time()
-
   ### ERROR CHECKING AND ARGUMENT INITIALIZATION ###############################
 
-  chunk_size <- 10000000
-  iterations <- 1
+  start_time <- Sys.time()
 
 
-  ## Set up on.exit expression for errors
+  ## Input checking ------------------------------------------------------------
 
-  on.exit({
-    # Flush out any stray multicore processes
-    future_map(1:future::nbrOfWorkers(), ~.x)
+  # Check that quiet is a logical
+  if (!is.logical(quiet)) {
+    stop("The argument `quiet` must be a logical value (TRUE or FALSE).")
+  }
 
-    # Restore future global export limit
-    .Options$future.globals.maxSize <- NULL
-
-    # Print \n so error messages don't collide with progress messages
-    if (!quiet) message()
-  })
-
-
-  ## Prepare data.table and future variables
-
-  .datatable.aware = TRUE
-
-  property_ID <- start_date <- end_date <- col_split <- host_ID <- NULL
-
-  options(future.globals.maxSize = +Inf)
-
-
-  ## Check if table is daily or host
-
+  # Check if table is daily or host
   if (inherits(data, "strr_daily") | names(data)[1] == "property_ID") {
     daily <- TRUE
   } else if (inherits(data, "strr_host") | names(data)[1] == "host_ID") {
@@ -64,19 +40,54 @@ strr_expand <- function(data, quiet = FALSE) {
   } else stop("Input table must be of class `strr_daily` or `strr_host`.")
 
 
-  ## Check for maximum size
+  ## Check for maximum size ----------------------------------------------------
 
-  if (nrow(data) > 300000000) {
-    if ({
-      data %>%
-        mutate(rows = as.numeric(end_date) - as.numeric(start_date) + 1) %>%
-        pull(.data$rows) %>%
-        sum()
-      } > 2294398000) {
-      stop(
-        "Output table will exceed maximum R data frame size (2 billion rows).")
+  if (nrow(data) > 3e8) {
+    if (sum(as.numeric(data$end_date) - as.numeric(data$start_date) + 1) >
+        2294398000) {
+      stop("Output table will exceed maximum data frame size (2 billion rows).")
     }
   }
+
+
+  ## Prepare data.table and future variables -----------------------------------
+
+  .datatable.aware = TRUE
+
+  # Silence R CMD check for data.table fields
+  property_ID <- start_date <- end_date <- col_split <- host_ID <- NULL
+
+  if (requireNamespace("future", quietly = TRUE) &&
+      requireNamespace("furrr", quietly = TRUE)) {
+
+    # Remove limit on globals size
+    options(future.globals.maxSize = +Inf)
+
+    # Set up on.exit expression for errors
+    on.exit({
+      # Flush out any stray multicore processes
+      furrr::future_map(1:future::nbrOfWorkers(), ~.x)
+
+      # Restore future global export limit
+      .Options$future.globals.maxSize <- NULL
+
+    })
+  }
+
+
+  ## Prepare batches and progress reporting ------------------------------------
+
+  # Enable progress bars if quiet == FALSE
+  progress <- !quiet
+
+  # Disable progress bars if {progressr} is not installed
+  if (!requireNamespace("progressr", quietly = TRUE)) {
+    progress <- FALSE
+    .strr_env$pb <- function() NULL
+  }
+
+  chunk_size <- 1e7
+  iterations <- 1
 
 
   ### SET BATCH PROCESSING STRATEGY ############################################
@@ -86,117 +97,126 @@ strr_expand <- function(data, quiet = FALSE) {
     iterations <- ceiling(nrow(data) / chunk_size)
 
     if (daily) {
-      helper_progress_message(
-        "Daily table identified. It will be processed in ", iterations,
-        " batches.")
+      helper_message("Daily table identified. It will be processed in ",
+                     iterations, " batches.")
     } else {
-      helper_progress_message(
-        "Host table identified. It will be processed in ", iterations,
-        " batches.")
+      helper_message("Host table identified. It will be processed in ",
+                     iterations, " batches.")
     }
   } else {
-    if (daily) {
-      helper_progress_message("Daily table identified.")
-    } else {
-      helper_progress_message("Host table identified.")
-    }
+    if (daily) {helper_message("Daily table identified.")
+    } else helper_message("Host table identified.")
   }
 
 
   ### STORE EXTRA FIELDS AND TRIM DATA #########################################
 
-  setDT(data)
+  data.table::setDT(data)
 
   if (daily) {
 
     # These fields are per-property
-    join_fields <- data[, last(.SD), by = property_ID
+    join_fields <- data[, data.table::last(.SD), by = property_ID
                         ][, c("start_date", "end_date", "status", "booked_date",
                               "price", "res_ID") := NULL]
 
-    # These fields are per-property, per-date
-    add_fields <-
-      data[, c("property_ID", "start_date", "status", "booked_date", "price",
-               "res_ID")]
-
-    setnames(add_fields, "start_date", "date")
-
     # Just keep data necessary for expansion
-    data <- data[, .(property_ID, start_date, end_date)]
+    data[, c("host_ID", "listing_type", "housing", "country", "region", "city"
+             ) := NULL]
 
   }
 
 
-
   ### PROCESS FOR SMALL TABLE ##################################################
 
-  # Just run strr_expand_helper directly
   if (iterations == 1) {
 
-    helper_progress_message("(1/2) Beginning expansion, using {helper_plan()}.",
-                            .type = "progress")
+    helper_message("(1/2) Expanding table, using {helper_plan()}.")
 
-    data <- strr_expand_helper(data, daily, quiet)
+    if (progress) {
 
-  } else {
+      handler_strr("Expanding row")
+
+      progressr::with_progress({
+
+        # Initialize progress bar
+        .strr_env$pb <- progressr::progressor(steps = nrow(data))
+
+        data <- helper_expand(data, daily)
+
+      })
+
+    } else data <- helper_expand(data, daily)
 
 
   ### PROCESS FOR LARGE TABLE ##################################################
 
+  } else {
+
     # Split data into 10-million-line chunks
-    data_list <- list()
-    length(data_list) <- iterations
+    data_list <- vector("list", iterations)
 
     # Process each batch sequentially
     for (i in seq_len(iterations)) {
 
-      helper_progress_message("(", i, "/", iterations + 1, ") Expanding batch ",
-        i, ", using {helper_plan()}.", .type = "progress")
+      helper_message("(", i, "/", iterations + 1, ") Expanding batch ", i,
+                     ", using {helper_plan()}.")
 
-      data_list[[i]] <-
-        data %>%
-        slice(((i - 1) * chunk_size + 1):(i * chunk_size)) %>%
-        strr_expand_helper(daily, quiet)
+      range_1 <- (i - 1) * chunk_size + 1
+      range_2 <- min(i * chunk_size, nrow(data))
+
+      if (progress) {
+
+        handler_strr("Expanding row")
+
+        progressr::with_progress({
+
+          # Initialize progress bar
+          .strr_env$pb <-
+            progressr::progressor(steps = nrow(data[range_1:range_2]))
+
+          data_list[[i]] <- helper_expand(data[range_1:range_2], daily)
+
+        })
+
+      } else data_list[[i]] <- helper_expand(data[range_1:range_2], daily)
 
     }
 
     # Bind batches together
-    data <- rbindlist(data_list)
+    data <- data.table::rbindlist(data_list)
 
   }
 
 
   ### REJOIN TO ADDITIONAL FIELDS, THEN ARRANGE COLUMNS ########################
 
-  helper_progress_message(
-    "(", iterations + 1, "/", iterations + 1,
-    ") Arranging table and joining additional fields.", .type = "open")
+  helper_message("(", iterations + 1, "/", iterations + 1, ") Arranging table.",
+                 .type = "open")
 
   if (daily) {
 
-    data <-
-      setDT(data)[order(property_ID, date)] %>%
-      # Join fields which need to be duplicated for specific date ranges
-      left_join(add_fields, by = c("property_ID", "date")) %>%
-      tidyr::fill(.data$status:.data$res_ID) %>%
-      # Join fields which need to be duplicated for specific properties
-      left_join(join_fields, by = "property_ID") %>%
-      select(.data$property_ID, .data$date, everything(), -.data$start_date,
-             -.data$end_date) %>%
-      as_tibble()
+    data <- data.table::setDT(data)[order(property_ID, date)]
+
+    data <- dplyr::left_join(data, join_fields, by = "property_ID")
+
+    data <- dplyr::as_tibble(data)
+
+    data <- dplyr::select(
+      data, .data$property_ID, .data$date, dplyr::everything())
 
   } else {
 
-    data <-
-      setDT(data)[order(host_ID, date)] %>%
-      select(.data$host_ID, .data$date, everything(), -.data$start_date,
-             -.data$end_date)
+    data <- data.table::setDT(data)[order(host_ID, date)]
+
+    data <- dplyr::as_tibble(data)
+
+    data <- dplyr::select(data, .data$host_ID, .data$date, dplyr::everything())
 
   }
 
-  helper_progress_message(
-    "(", iterations + 1, "/", iterations + 1,
-    ") Table arranged and additional fields joined.", .type = "close")
+  helper_message("(", iterations + 1, "/", iterations + 1, ") Table arranged.",
+                 .type = "close")
 
 
   ### SET CLASS OF OUTPUT ######################################################
@@ -210,9 +230,11 @@ strr_expand <- function(data, quiet = FALSE) {
 
   ### OUTPUT DATA FRAME ########################################################
 
-  helper_progress_message("Expansion complete.", .type = "final")
+  helper_message("Expansion complete.", .type = "final")
 
-  on.exit(.Options$future.globals.maxSize <- NULL)
+  if (requireNamespace("future", quietly = TRUE)) {
+    on.exit(.Options$future.globals.maxSize <- NULL)
+  }
 
   return(data)
 }
@@ -229,16 +251,53 @@ strr_expand <- function(data, quiet = FALSE) {
 #' are recognized.
 #' @param daily_flag A logical scalar. Is the input table a daily table (TRUE)
 #' or a host table (FALSE)?
-#' @param quiet A logical scalar. Should the function execute quietly, or should
-#' it return status updates throughout the function (default)?
 #' @return A table with one row per date and all other fields returned
 #' unaltered.
+#' @importFrom dplyr %>%
 
-strr_expand_helper <- function(data, daily_flag, quiet) {
+helper_expand <- function(data, daily_flag) {
+
+  ### INITIALIZE OBJECTS #######################################################
 
   .datatable.aware = TRUE
+
   property_ID <- start_date <- end_date <- col_split <- host_ID <- NULL
-  setDT(data)
+
+  data.table::setDT(data)
+
+
+  ## Define map_* --------------------------------------------------------------
+
+  if (requireNamespace("future", quietly = TRUE) &&
+      requireNamespace("furrr", quietly = TRUE)) {
+
+    map_dfr <- furrr::future_map_dfr
+
+  } else map_dfr <- purrr::map_dfr
+
+
+  ### DEFINE FUNCTION TO BE MAPPED #############################################
+
+  expand_fun <- function(.x) {
+
+    # Iterate progress bar
+    if (requireNamespace("progressr", quietly = TRUE)) {
+      .strr_env$pb(amount = nrow(.x))
+    }
+
+    data.table::setDT(.x)
+
+    # Add new date field
+    .x[, date := list(list(start_date:end_date)), by = 1:nrow(.x)]
+
+    # Unnest
+    .x <-
+      .x[, lapply(.SD, unlist), by = 1:nrow(.x)
+         ][, c("nrow", "start_date", "end_date") := NULL]
+
+    .x[, date := as.Date(date, origin = "1970-01-01")]
+  }
+
 
 
   ### SPLIT DATA ###############################################################
@@ -258,31 +317,13 @@ strr_expand_helper <- function(data, daily_flag, quiet) {
   ### EXPAND TABLE #############################################################
 
   # Make sure data.table is single-threaded within the helper
-  threads <- setDTthreads(1)
+  threads <- data.table::setDTthreads(1)
 
-  data <-
-    data_list %>%
-    future_map_dfr(~{
-
-      setDT(.x)
-
-      # Add new date field
-      .x[, date := list(list(start_date:end_date)), by = 1:nrow(.x)]
-
-      # Unnest
-      .x[, lapply(.SD, unlist), by = 1:nrow(.x)][, nrow := NULL] %>%
-        as_tibble() %>%
-        mutate(date = as.Date(.data$date, origin = "1970-01-01"))
-    },
-    # Suppress progress bar if quiet == TRUE or the plan is remote
-    .progress = helper_progress()
-    )
+  # Run function
+  data <- map_dfr(data_list, expand_fun)
 
   # Restore DT threads
-  setDTthreads(threads)
-
-  # Flush out any stray multicore processes
-  future_map(1:future::nbrOfWorkers(), ~.x)
+  data.table::setDTthreads(threads)
 
   return(data)
 
