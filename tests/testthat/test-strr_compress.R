beb2dd5558e600d9fb1f5a2c74baa499a30bf1fa
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-07-05 09:26:22 -0400

    More tests

--- tests/testthat/test-strr_compress.R
+++ tests/testthat/test-strr_compress.R
@@ -119,13 +119,15 @@ test_that("function completes with no errors", {
   # expect_equal(nrow(strr_compress(host)), 7)
 })
 
-# test_that("function sets correct start/end dates", {
-#   # Start date
-#   expect_equal(pull(strr_compress(daily), start_date)[2], as.Date("2018-04-06"))
-#   # End date
-#   expect_equal(pull(strr_compress(daily), end_date)[2], as.Date("2018-04-07"))
-# })
-#
+test_that("function sets correct start/end dates", {
+  # Start date
+  expect_equal(pull(strr_compress(daily_compress), start_date)[2],
+               as.Date("2018-04-06"))
+  # End date
+  expect_equal(pull(strr_compress(daily_compress), end_date)[2],
+               as.Date("2018-04-07"))
+})
+
 # test_that("month/year boundaries are handled properly", {
 #   expect_equal(nrow(strr_compress(daily_2)), 7)
 #   expect_equal(nrow(strr_compress(daily_3)), 7)
